18b9dd68f856cc5b43879699cda9e21ce8b2e479
Tao Dong <taodong@google.com>
2019-11-25 15:37:54 -0800

    Add validateEmail(email)

--- lib/string_demo.dart
+++ lib/string_demo.dart
@@ -2,3 +2,6 @@ String skipLastChar(String text) {
   return text.substring(0, text.length - 1);
 }
 
+bool validateEmail(String email) {
+  return email.contains(new RegExp(r'.+@.+\.[a-z]*'));
+}
--- test.dart
+++ test.dart
@@ -1,9 +1,16 @@
 import 'package:test/test.dart';
-import 'package:dart_text_processing_demo/main.dart';
+import 'package:dart_text_processing_demo/string_demo.dart';
 
 void main() {
   test("skipLastChar(text) removes the last character from the string", () {
     var string = 'Hi 🇩🇰';
     expect(skipLastChar(string), equals('Hi '));
+  });
+
+  test("validateEmail(email) checks if the email is likely to be valid", () {
+    var email = 'café@';
+    expect(validateEmail(email), equals(false));
+    email = 'café@google.com';
+    expect(validateEmail(email), equals(true));
   });
 }
